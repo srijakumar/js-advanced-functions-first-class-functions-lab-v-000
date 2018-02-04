2583ca9abdc33a6b816d6f35c00ad7163bc3fa81
Srija S <srijakumar7@gmail.com>
2018-02-04 16:45:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,11 @@ const returnLastTwoDrivers = function(drivers){
 
 const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers]
 
-const createFareMultiplier= function fareDoubler(fare){
-  return fare * 2;
+const createFareMultiplier = function (multiplier){
+  function(value){
+    return value * multiplier;
+  }
 }
+
 
 
