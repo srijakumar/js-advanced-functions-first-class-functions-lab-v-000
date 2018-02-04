5a5ae02b628eab69443d2e9958aaa99d4cd26160
Srija S <srijakumar7@gmail.com>
2018-02-04 16:16:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 const returnFirstTwoDrivers= function(drivers){
   return drivers.slice(0,2);
 }
+
+const returnLastTwoDrivers = function(drivers){
+  const n = drivers.length;
+  return drivers.slice(n-2, n);
+}
